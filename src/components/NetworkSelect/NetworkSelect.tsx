2ce27abbc62f177d888fc317687c30011533a0c6
diko <diko93@yandex.ru>
2025-03-18 17:09:26 +0500

    fix: added default network check to sepolia

--- src/components/NetworkSelect/NetworkSelect.tsx
+++ src/components/NetworkSelect/NetworkSelect.tsx
@@ -1,24 +1,49 @@
+import React from 'react'
 import { useChainId, useSwitchChain } from 'wagmi'
 import { SUPPORTED_NETWORKS } from '../../config/networks'
 import { getFactoryAddress } from '../../config/dao'
 import styles from './NetworkSelect.module.css'
 
-export function NetworkSelect() {
+interface NetworkSelectProps {
+  onSwitchError?: (error: Error) => void;
+}
+
+export function NetworkSelect({ onSwitchError }: NetworkSelectProps = {}) {
   const chainId = useChainId()
-  const { switchChain } = useSwitchChain()
+  const { switchChain, isPending, error } = useSwitchChain()
+
+  // If there's an error, pass it to the parent component
+  React.useEffect(() => {
+    if (error && onSwitchError) {
+      onSwitchError(error);
+    }
+  }, [error, onSwitchError]);
+
+  const handleNetworkChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newChainId = Number(e.target.value)
+    const networkConfig = getFactoryAddress(newChainId)
+    if (networkConfig.isAvailable) {
+      try {
+        await switchChain({ chainId: newChainId as 1 | 11155111 | 56 | 8453 | 42161 | 43114 | 137 | 10 | 100 | 81457 | 534352 })
+      } catch (err) {
+        // Error will be handled by the useEffect above
+        console.error('Network switch failed:', err)
+      }
+    }
+  }
 
   return (
     <div className={styles.selectContainer}>
+      {isPending && (
+        <div className={styles.loadingOverlay}>
+          <span className={styles.loadingText}>Switching network...</span>
+        </div>
+      )}
       <select
         value={chainId}
-        onChange={(e) => {
-          const newChainId = Number(e.target.value)
-          const networkConfig = getFactoryAddress(newChainId)
-          if (networkConfig.isAvailable) {
-            switchChain?.({ chainId: newChainId as 1 | 11155111 | 56 | 8453 | 42161 | 43114 | 137 | 10 | 100 | 81457 | 534352 })
-          }
-        }}
+        onChange={handleNetworkChange}
         className={styles.select}
+        disabled={isPending}
       >
         {SUPPORTED_NETWORKS.map((network) => {
           const networkConfig = getFactoryAddress(network.id)
